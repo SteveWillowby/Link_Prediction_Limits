eb00d7b8c4fcea5a42f8f21a7e8ea600de70215d
Steve Willowby <justus.i.hibshman@gmail.com>
2021-09-29 15:19:01 -0400

    Dataset loader for KG completion.

--- get_OGB_max_scores.py
+++ get_OGB_max_scores.py
@@ -17,6 +17,69 @@ graph_classification_root = dataset_base + "/PCQM4M"
 link_pred_root = dataset_base + "/WikiKG90M"
 node_classification_root = dataset_base + "/MAG240M"
 
+def link_pred_dataset():
+    dataset = WikiKG90MDataset(root=link_pred_root)
+
+    train_hrt = dataset.train_hrt
+    (num_triples, _) = train_hrt.shape
+
+    nodes = set()
+
+    # Get edges and flatten edge types.
+    print("Loading edges...")
+    edges = set([(train_hrt[i,0], train_hrt[i,2]) for i in range(0, num_triples)])
+    edge_types = {edge: set() for edge in edges}
+    print("Flattening edge types...")
+    for i in range(0, num_triples):
+        edge = (train_hrt[i,0], train_hrt[i,2])
+        t = train_hrt[i,1]
+        edge_types[edge].add(t)
+
+    next_type_id = 0
+    edge_type_combo_dict = {}
+    for edge in edges:
+        nodes.add(edge[0])
+        nodes.add(edge[1])
+        types = sorted(tuple(edge_types[edge]))
+        if types not in edge_type_combo_dict:
+            edge_type_combo_dict[types] = next_type_id
+            next_type_id += 1
+        edge_types[edge] = edge_type_combo_dict[types]
+    print("  ... %d total flattened edge types." % len(edge_type_combo_dict))
+
+    valid_task = dataset.valid_dict['h,r->t']
+    hr = valid_task['hr']
+    t_candidate = valid_task['t_candidate']
+    t_correct_index = valid_task['t_correct_index']
+    (num_predictions, _) = hr.shape
+
+    print("Num nodes before adding validation nodes: %d" % len(nodes))
+    for i in range(0, num_predictions):
+        nodes.add(hr[i,0])
+        for j in range(0, 1001):
+            nodes.add(t_candidate[i,j])
+
+    assert max(nodes) == len(nodes) - 1
+    assert min(nodes) == 0
+    print("  ... obtained %d nodes" % len(nodes))
+
+    directed = True
+
+    # New Way:
+    # graph = GraphView(nodes, edges, directed, edge_types=edge_types)
+    # node_coloring = Coloring([0 for _ in nodes])
+
+    # Old Way:
+    ons = {n: set() for n in nodes}
+    ins = {n: set() for n in nodes}
+    for (a, b) in edges:
+        ons[a].add(b)
+        ins[b].add(a)
+    graph = (ons, ins, directed, edge_types)
+    node_coloring = [0 for _ in nodes]
+
+    return (graph, node_coloring, hr, t_candidate, t_correct_index)
+
 def node_classifier_dataset():
     dataset = MAG240MDataset(root=node_classification_root)
 
@@ -146,8 +209,17 @@ def get_max_score_for_node_classification(graph, node_colors, validation_node_la
     print("  Max Possible Validation Accuracy is: %f   (%d / %d)" % (float(M) / float(S), M, S))
 
 if __name__ == "__main__":
-    print("Loading MAG Graph (Node Classification Graph)...")
-    (graph, node_colors, validation_node_labels) = node_classifier_dataset()
-    print("...Graph Loaded")
-    print("Getting Max Possible Validation Score...")
-    get_max_score_for_node_classification(graph, node_colors, validation_node_labels)
+    task = "Link Pred"  # "Link Pred", "Node Classification", and "Graph Classification"
+    if task == "Link Pred":
+        (graph, node_colors, hr, t_candidate, t_correct_idx) = link_pred_dataset()
+
+    elif task == "Node Classification":
+        print("Loading MAG Graph (Node Classification Graph)...")
+        (graph, node_colors, validation_node_labels) = node_classifier_dataset()
+        print("...Graph Loaded")
+        print("Getting Max Possible Validation Score...")
+        get_max_score_for_node_classification(graph, node_colors, validation_node_labels)
+    elif task == "Graph Classification":
+        pass
+    else:
+        raise ValueError("Error! Unknown task '%s'" % task)
