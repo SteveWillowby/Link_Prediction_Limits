4e96c53742816b084d63c86421d4369157a9ea54
Steve Willowby <justus.i.hibshman@gmail.com>
2022-01-25 15:02:02 -0500

    Increased margin -- should look into whether or not this is just needed due to fp rounding errors.

--- max_score_functions.py
+++ max_score_functions.py
@@ -35,7 +35,8 @@ def get_max_AUPR(class_info):
             addition = (a * a) / c
         else:
             addition = ((a * a) / c) * (1.0 + ((b / a) - (d / c)) * math.log((d + c) / d))
-            assert_margin = 0.0001
+            # TODO: Look into the meaning of this assertion margin.
+            assert_margin = 0.0002
             assert addition + assert_margin >= (a * a) / c
             assert addition - assert_margin <= a * (((a + b) / (c + d)) + (b / c)) / 2.0
 
