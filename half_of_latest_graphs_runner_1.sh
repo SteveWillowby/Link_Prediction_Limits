245897aa52032f7d74f4e6f70084ee37ca4c3e33
Steve Willowby <justus.i.hibshman@gmail.com>
2022-02-28 18:36:45 -0500

    runners for preliminary testing

---
+++ half_of_latest_graphs_runner_1.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+time python3 test_local_graphs.py np=58 ntpp=1 k=all py_iso=false repercent=10 nepercent=100 num_runs=2 hash_endpoints=true g=y2h_ppi
+
+time python3 test_local_graphs.py np=58 ntpp=1 k=all py_iso=false repercent=10 nepercent=100 num_runs=2 hash_endpoints=true g=collins_yeast
+
+time python3 test_local_graphs.py np=58 ntpp=1 k=all py_iso=false repercent=10 nepercent=100 num_runs=2 hash_endpoints=true g=faculty_business
+time python3 test_local_graphs.py np=58 ntpp=1 k=all py_iso=false repercent=10 nepercent=100 num_runs=2 hash_endpoints=true g=faculty_comp_sci
+time python3 test_local_graphs.py np=58 ntpp=1 k=all py_iso=false repercent=10 nepercent=100 num_runs=2 hash_endpoints=true g=faculty_history
+
+time python3 test_local_graphs.py np=58 ntpp=1 k=all py_iso=false repercent=10 nepercent=100 num_runs=2 hash_endpoints=true g=roman_roads_p
+time python3 test_local_graphs.py np=58 ntpp=1 k=all py_iso=false repercent=10 nepercent=100 num_runs=2 hash_endpoints=true g=roman_roads_u
+
+time python3 test_local_graphs.py np=58 ntpp=1 k=all py_iso=false repercent=10 nepercent=100 num_runs=2 hash_endpoints=true g=powergrid
+
+time python3 test_local_graphs.py np=58 ntpp=1 k=all py_iso=false repercent=10 nepercent=100 num_runs=2 hash_endpoints=true g=US_500_airports_l
+
+time python3 test_local_graphs.py np=58 ntpp=1 k=all py_iso=false repercent=10 nepercent=100 num_runs=2 hash_endpoints=true g=US_airports_2010_u
