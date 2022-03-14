50bd88a1171f2cb424074f9f5a6cb86bb8899066
Steve Willowby <justus.i.hibshman@gmail.com>
2022-03-14 16:22:49 -0400

    Changed to run on just 7 cores - avg. degree ranges from 8 to 9 - percent of possible edges ranges from 1.1 to 3.2

---
+++ ER_edge_ratio_test.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_100_54
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_100_67
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_100_80
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_100_93
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_100_106
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_100_119
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_100_132
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_100_145
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_100_158
+
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_200_219
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_200_271
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_200_323
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_200_376
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_200_428
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_200_480
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_200_532
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_200_585
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_200_637
+
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_300_493
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_300_611
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_300_729
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_300_847
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_300_964
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_300_1082
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_300_1200
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_300_1317
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_300_1435
+
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_400_878
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_400_1087
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_400_1297
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_400_1506
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_400_1716
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_400_1925
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_400_2135
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_400_2344
+time python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=false concealed=10 considered=auto num_runs=10 hash_endpoints=true g=ER_400_2554
