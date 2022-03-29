0cfe2f3387bf91a17f0188a6672ab0a9091a8ec0
Steve Willowby <justus.i.hibshman@gmail.com>
2022-03-29 18:35:19 -0400

    another runner

---
+++ runners/WS_rewiring_test.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+echo "# WS_500_4_<beta>"
+echo "# 0"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_500_4_0
+echo "# 0.1"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_500_4_0.1
+echo "# 0.2"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_500_4_0.2
+echo "# 0.3"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_500_4_0.3
+echo "# 0.4"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_500_4_0.4
+echo "# 0.5"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_500_4_0.5
+echo "# 0.6"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_500_4_0.6
+echo "# 0.7"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_500_4_0.7
+echo "# 0.8"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_500_4_0.8
+echo "# 0.9"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_500_4_0.9
+echo "# 1"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_500_4_1
+
+echo "# WS_400_4_<beta>"
+echo "# 0"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_4_0
+echo "# 0.1"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_4_0.1
+echo "# 0.2"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_4_0.2
+echo "# 0.3"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_4_0.3
+echo "# 0.4"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_4_0.4
+echo "# 0.5"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_4_0.5
+echo "# 0.6"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_4_0.6
+echo "# 0.7"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_4_0.7
+echo "# 0.8"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_4_0.8
+echo "# 0.9"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_4_0.9
+echo "# 1"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_4_1
+
+echo "# WS_400_3_<beta>"
+echo "# 0"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_3_0
+echo "# 0.1"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_3_0.1
+echo "# 0.2"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_3_0.2
+echo "# 0.3"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_3_0.3
+echo "# 0.4"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_3_0.4
+echo "# 0.5"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_3_0.5
+echo "# 0.6"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_3_0.6
+echo "# 0.7"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_3_0.7
+echo "# 0.8"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_3_0.8
+echo "# 0.9"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_3_0.9
+echo "# 1"
+python3 test_local_graphs.py np=7 ntpp=1 k=all py_iso=true repercent=5 nepercent=100 num_runs=1 hash_endpoints=true g=WS_400_3_1
