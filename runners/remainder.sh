bea17bbe64cbe6c2745b256cf608f09b5da4d7ec
Steve Willowby <justus.i.hibshman@gmail.com>
2022-06-30 16:24:03 -0400

    ...

---
+++ runners/remainder.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+time python3 test_local_graphs.py np=45 ntpp=1 k=all py_iso=true repercent=10 nepercent=100 num_runs=4 hash_endpoints=true g=species_1
+
+time python3 test_local_graphs.py np=45 ntpp=1 k=all py_iso=true repercent=10 nepercent=100 num_runs=4 hash_endpoints=true g=US_500_airports_u
+
+time python3 test_local_graphs.py np=45 ntpp=1 k=all py_iso=true repercent=10 nepercent=100 num_runs=4 hash_endpoints=true g=eucore
+
+time python3 test_local_graphs.py np=45 ntpp=1 k=all py_iso=true repercent=10 nepercent=100 num_runs=4 hash_endpoints=true g=roget_thesaurus
