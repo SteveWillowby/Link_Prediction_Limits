c42090ed62e35d9bdfdca778485d6d43ec434af5
SteveWillowby <blank_email>
2022-06-30 16:31:51 -0400

    ...

--- runners/remainder.sh
+++ runners/remainder.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-time python3 test_local_graphs.py np=45 ntpp=1 k=all py_iso=true repercent=10 nepercent=100 num_runs=4 hash_endpoints=true g=species_1
+time python3 test_local_graphs.py np=45 ntpp=1 k=all py_iso=true repercent=10 nepercent=100 num_runs=4 hash_endpoints=true g=species_1_brain
 
 time python3 test_local_graphs.py np=45 ntpp=1 k=all py_iso=true repercent=10 nepercent=100 num_runs=4 hash_endpoints=true g=US_500_airports_u
 
