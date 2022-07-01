4aad639bac1a1836f3867749b99835cc103fc219
Steve Willowby <justus.i.hibshman@gmail.com>
2022-07-01 16:22:31 -0400

    ...

--- runners/remainder.sh
+++ runners/remainder.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 
-time python3 test_local_graphs.py np=45 ntpp=1 k=all py_iso=true repercent=10 nepercent=100 num_runs=4 hash_endpoints=true g=species_1_brain
+# time python3 test_local_graphs.py np=45 ntpp=1 k=all py_iso=true repercent=10 nepercent=100 num_runs=4 hash_endpoints=true g=species_1_brain
 
-time python3 test_local_graphs.py np=45 ntpp=1 k=all py_iso=true repercent=10 nepercent=100 num_runs=4 hash_endpoints=true g=US_500_airports_u
+# time python3 test_local_graphs.py np=45 ntpp=1 k=all py_iso=true repercent=10 nepercent=100 num_runs=4 hash_endpoints=true g=US_500_airports_u
 
-time python3 test_local_graphs.py np=45 ntpp=1 k=all py_iso=true repercent=10 nepercent=100 num_runs=4 hash_endpoints=true g=eucore
+# time python3 test_local_graphs.py np=45 ntpp=1 k=all py_iso=true repercent=10 nepercent=100 num_runs=4 hash_endpoints=true g=eucore
 
-time python3 test_local_graphs.py np=45 ntpp=1 k=all py_iso=true repercent=10 nepercent=100 num_runs=4 hash_endpoints=true g=roget_thesaurus
+# time python3 test_local_graphs.py np=45 ntpp=1 k=all py_iso=true repercent=10 nepercent=100 num_runs=4 hash_endpoints=true g=roget_thesaurus
+
+time python3 test_local_graphs.py np=45 ntpp=1 k=all py_iso=true repercent=10 nepercent=100 num_runs=4 hash_endpoints=true g=mysql_calls
+
+time python3 test_local_graphs.py np=45 ntpp=1 k=all py_iso=true repercent=10 nepercent=100 num_runs=6 hash_endpoints=true g=citeseer
